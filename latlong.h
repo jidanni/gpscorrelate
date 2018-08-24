3c6465da2ba7c9308af4aa03c09c01609e624288
Dan Fandrich <dan@coneharvesters.com>
2018-08-24 18:15:57 +0200

    Removed a hack for pre-C99 compilers.
    C99 is the minimum documented supported standard.

--- latlong.h
+++ latlong.h
@@ -1,10 +1,5 @@
 #include <math.h>
 
-#ifndef NAN
-/* This is not available before C99 */
-#define NAN (0. / 0.)
-#endif
-
 int ParseLatLong(const char *latlongstr, struct GPSPoint* point);
 int MakeTrackFromLatLong(const struct GPSPoint* latlong, struct GPSTrack* track);
 int NumDecimals(const char *Decimal);
